6ff5064b135faed7578f540b2140d3b7269c8028
Marwan Aziz <marwan@marwanaziz.net>
2026-04-12 14:23:14 +0100

    initial commit

---
+++ City Scout iOS/City_Scout_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_Scout_iOSApp.swift
+//  City Scout iOS
+//
+//  Created by Marwan Aziz on 12/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct City_Scout_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ City Scout iOSTests/City_Scout_iOSTests.swift
@@ -0,0 +1,18 @@
+//
+//  City_Scout_iOSTests.swift
+//  City Scout iOSTests
+//
+//  Created by Marwan Aziz on 12/04/2026.
+//
+
+import Testing
+
+struct City_Scout_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
